a461662d1f9bc57e7a8dd231b38780c5ce1e2586
aakarshan-raj <aakarshan7raj@gmail.com>
2024-01-10 20:45:06 +0530

    program 1 done

--- 2. Program 1/src/main.cpp
+++ 2. Program 1/src/main.cpp
@@ -10,14 +10,28 @@ const int FrameRate = 1000;
 const char *Title = "Bound It!";
 const char *BottomText = "Sample Text";
 
-void read_file(char *, std::map<std::string, std::pair<std::shared_ptr<sf::Shape>, std::pair<float, float>>> &,
-               std::shared_ptr<sf::RenderWindow> &);
+struct font
+{
+    sf::Font f;
+    float size;
+    int R, G, B;
+};
+
+void read_file(char *,
+               std::map<std::string,
+                        std::pair<std::pair<
+                                      std::shared_ptr<sf::Shape>,
+                                      std::shared_ptr<sf::Text>>,
+                                  std::pair<float, float>>> &,
+               std::shared_ptr<sf::RenderWindow> &,
+               font &);
 
 int main()
 {
     std::shared_ptr<sf::RenderWindow> window = std::make_shared<sf::RenderWindow>();
-    std::map<std::string, std::pair<std::shared_ptr<sf::Shape>, std::pair<float, float>>> Stuff;
-    read_file("config/config", Stuff, window);
+    std::map<std::string, std::pair<std::pair<std::shared_ptr<sf::Shape>, std::shared_ptr<sf::Text>>, std::pair<float, float>>> Stuff;
+    font f1;
+    read_file("config/config", Stuff, window, f1);
 
     window->setFramerateLimit(FrameRate);
 
@@ -35,24 +49,29 @@ int main()
         window->clear();
         for (auto &[key, value] : Stuff)
         {
-            window->draw(*(value.first));
-            value.first->move(value.second.first, value.second.second);
-            if ((value.first)->getPosition().x < 0)
+            window->draw(*(value.first.first));
+            value.first.first->move(value.second.first, value.second.second);
+
+            if ((value.first.first)->getPosition().x < 0)
             {
                 value.second.first *= -1;
             }
-            if ((value.first)->getPosition().y < 0)
+            if ((value.first.first)->getPosition().y < 0)
             {
                 value.second.second *= -1;
             }
-            if ((value.first)->getPosition().x > window->getSize().x)
+            if ((value.first.first)->getPosition().x + value.first.first->getLocalBounds().width > window->getSize().x)
             {
                 value.second.first *= -1;
             }
-            if ((value.first)->getPosition().y > window->getSize().y)
+            if ((value.first.first)->getPosition().y + value.first.first->getLocalBounds().height > window->getSize().y)
             {
                 value.second.second *= -1;
             }
+            value.first.second->setPosition(value.first.first->getPosition().x+(value.first.first->getLocalBounds().width/2)-value.first.second->getLocalBounds().width/2,
+                                            value.first.first->getPosition().y+(value.first.first->getLocalBounds().height/2)-value.first.second->getLocalBounds().height/2);
+
+            window->draw(*value.first.second);
         }
 
         window->display();
@@ -60,8 +79,8 @@ int main()
     return 0;
 }
 
-void read_file(char *filepath, std::map<std::string, std::pair<std::shared_ptr<sf::Shape>, std::pair<float, float>>> &Stuff,
-               std::shared_ptr<sf::RenderWindow> &window)
+void read_file(char *filepath, std::map<std::string, std::pair<std::pair<std::shared_ptr<sf::Shape>, std::shared_ptr<sf::Text>>, std::pair<float, float>>> &Stuff,
+               std::shared_ptr<sf::RenderWindow> &window, font &font)
 {
     std::ifstream file_handler;
     file_handler.open(filepath);
@@ -72,8 +91,7 @@ void read_file(char *filepath, std::map<std::string, std::pair<std::shared_ptr<s
         {
             float width, height;
             file_handler >> width >> height;
-            std::cout << "Window " << width << " " << height << std::endl;
-            window->create(sf::VideoMode(width, height), "hi");
+            window->create(sf::VideoMode(width, height), "bounce");
         }
         else if (line == "Font")
         {
@@ -83,7 +101,15 @@ void read_file(char *filepath, std::map<std::string, std::pair<std::shared_ptr<s
             int G;
             int B;
             file_handler >> font_path >> size >> R >> G >> B;
-            std::cout << "Font " << font_path << " " << size << " " << R << " " << G << " " << B << std::endl;
+            if (!font.f.loadFromFile(font_path))
+            {
+                std::cout << "error in importing font" << std::endl;
+                exit(-1);
+            }
+            font.size = size;
+            font.R = R;
+            font.G = G;
+            font.B = B;
         }
         else if (line == "Circle")
         {
@@ -91,13 +117,13 @@ void read_file(char *filepath, std::map<std::string, std::pair<std::shared_ptr<s
             float x, y, x_speed, y_speed, radius;
             int R, G, B;
             file_handler >> name >> x >> y >> x_speed >> y_speed >> R >> G >> B >> radius;
-            std::cout << "Circle " << name << " " << x << " " << y << " " << x_speed << " "
-                      << y_speed << " " << R << " " << G << " " << B << " " << radius << std::endl;
             std::shared_ptr<sf::Shape> c1 = std::make_shared<sf::CircleShape>(radius);
             c1->setPosition(x, y);
-            c1->move(x_speed, y_speed);
             c1->setFillColor(sf::Color(R, G, B));
-            Stuff.insert(std::make_pair(name, std::pair(c1, std::pair(x_speed, y_speed))));
+            std::shared_ptr<sf::Text> f1 = std::make_shared<sf::Text>(name, font.f);
+            f1->setColor(sf::Color(font.R, font.G, font.B));
+            f1->setCharacterSize(font.size);
+            Stuff.insert(std::make_pair(name, std::pair(std::make_pair(c1, f1), std::pair(x_speed, y_speed))));
         }
         else if (line == "Rectangle")
         {
@@ -106,13 +132,13 @@ void read_file(char *filepath, std::map<std::string, std::pair<std::shared_ptr<s
             float x, y, x_speed, y_speed, width, height;
             int R, G, B;
             file_handler >> name >> x >> y >> x_speed >> y_speed >> R >> G >> B >> width >> height;
-            std::cout << "Rectangle" << name << " " << x << " " << y << " " << x_speed << " "
-                      << y_speed << " " << R << " " << G << " " << B << " " << width << " " << height << std::endl;
             std::shared_ptr<sf::Shape> r1 = std::make_shared<sf::RectangleShape>(sf::Vector2f(width, height));
             r1->setPosition(x, y);
-            r1->move(x_speed, y_speed);
             r1->setFillColor(sf::Color(R, G, B));
-            Stuff.insert(std::make_pair(name, std::pair(r1, std::pair(x_speed, y_speed))));
+            std::shared_ptr<sf::Text> f1 = std::make_shared<sf::Text>(name, font.f);
+            f1->setColor(sf::Color(font.R, font.G, font.B));
+            f1->setCharacterSize(font.size);
+            Stuff.insert(std::make_pair(name, std::pair(std::make_pair(r1, f1), std::pair(x_speed, y_speed))));
         }
         else
         {
@@ -121,3 +147,4 @@ void read_file(char *filepath, std::map<std::string, std::pair<std::shared_ptr<s
         }
     }
 }
+//Probaby the worst way to do it
