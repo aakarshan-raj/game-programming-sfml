bdcd0e5be68e535388b4aacf5dc20415d61d05f4
aakarshan-raj <aakarshan7raj@gmail.com>
2024-01-21 14:43:00 +0530

    user input

--- 3. Program 2/src/Game.cpp
+++ 3. Program 2/src/Game.cpp
@@ -1,5 +1,5 @@
 #include "Game.h"
-
+#include <iostream>
 Game::Game(const std::string &configFile)
 {
 	init(configFile);
@@ -7,7 +7,6 @@ Game::Game(const std::string &configFile)
 
 void Game::run()
 {
-	spawnPlayer();
 	while (m_running)
 	{
 		m_entities.update();
@@ -28,8 +27,9 @@ void Game::run()
 
 void Game::init(const std::string &config)
 {
-	 m_window.create(sf::VideoMode(1200,800),"hi");
-	 
+	m_window.create(sf::VideoMode(1200, 800), "hi");
+	m_window.setFramerateLimit(60);
+	spawnPlayer();
 }
 
 void Game::setPaused(bool paused)
@@ -38,20 +38,87 @@ void Game::setPaused(bool paused)
 
 void Game::sMovement()
 {
-	// All movements here
+
+	if (m_player->cInput->up == true)
+	{
+		if (m_player->cShape->circle.getPosition().y - m_player->cCollision->radius > 0)
+			m_player->cShape->circle.move(0, -10);
+	}
+	if (m_player->cInput->down == true)
+	{
+		if (m_player->cShape->circle.getPosition().y + m_player->cCollision->radius < 800)
+			m_player->cShape->circle.move(0, 10);
+	}
+	if (m_player->cInput->left == true)
+	{
+		if (m_player->cShape->circle.getPosition().x - m_player->cCollision->radius > 0)
+			m_player->cShape->circle.move(-10, 0);
+	}
+	if (m_player->cInput->right == true)
+	{
+		if (m_player->cShape->circle.getPosition().x + m_player->cCollision->radius <1200)
+			m_player->cShape->circle.move(10, 0);
+	}
 }
 
 void Game::sUserInput()
 {
 	// handle all user input, close, pause, up,down,left,right, mouse click, only set state here. No movements here
 	sf::Event event;
-	while(m_window.pollEvent(event)){
-		if(event.type == sf::Event::Closed){
-				m_window.close();			
+	while (m_window.pollEvent(event))
+	{
+		if (event.type == sf::Event::Closed)
+		{
+			m_window.close();
 		}
-		if(event.type == sf::Event::KeyPressed){
-			if(event.key.code == sf::Keyboard::Q){
-				m_window.close();
+		if (event.type == sf::Event::KeyPressed)
+		{
+			if (event.key.code == sf::Keyboard::W)
+			{
+				std::cout << "W pressed" << std::endl;
+				m_player->cInput->up = true;
+			}
+			if (event.key.code == sf::Keyboard::S)
+			{
+				std::cout << "S pressed" << std::endl;
+				m_player->cInput->down = true;
+			}
+			if (event.key.code == sf::Keyboard::A)
+			{
+				std::cout << "A pressed" << std::endl;
+				m_player->cInput->left = true;
+			}
+			if (event.key.code == sf::Keyboard::D)
+			{
+				std::cout << "D pressed" << std::endl;
+				m_player->cInput->right = true;
+			}
+		}
+		if (event.type == sf::Event::KeyReleased)
+		{
+			if (event.key.code == sf::Keyboard::W)
+			{
+				std::cout << "W released" << std::endl;
+
+				m_player->cInput->up = false;
+			}
+			if (event.key.code == sf::Keyboard::S)
+			{
+				std::cout << "S released" << std::endl;
+
+				m_player->cInput->down = false;
+			}
+			if (event.key.code == sf::Keyboard::A)
+			{
+				std::cout << "A released" << std::endl;
+
+				m_player->cInput->left = false;
+			}
+			if (event.key.code == sf::Keyboard::D)
+			{
+				std::cout << "D released" << std::endl;
+
+				m_player->cInput->right = false;
 			}
 		}
 	}
@@ -64,15 +131,16 @@ void Game::sLifeSpan()
 void Game::sRender()
 {
 	// Clear
-	//  Logic to change position
+	//  Logic to change position for the enemies
 	// Draw
 	// Display
 	m_window.clear();
-	for(const std::shared_ptr<Entity> &x:m_entities.getEntities()){
-	m_window.draw(x->cShape->circle);
+	m_player->cShape->circle.setRotation(100);
+	for (const std::shared_ptr<Entity> &x : m_entities.getEntities())
+	{
+		m_window.draw(x->cShape->circle);
 	}
 	m_window.display();
-
 }
 
 void Game::sCollision()
@@ -84,9 +152,11 @@ void Game::spawnPlayer()
 {
 
 	m_player = m_entities.addEntity("Player");
-	m_player->cShape =  std::make_shared<CShape>(100,50,sf::Color(100,200,255),sf::Color(100,200,255),3);
-	m_player->cShape->circle.setPosition(400,250);
-	
+	m_player->cTransform = std::make_shared<CTransform>(Vec2(400, 200), Vec2(0, 0), 0);
+	m_player->cCollision = std::make_shared<CCollision>(100);
+	m_player->cShape = std::make_shared<CShape>(m_player->cCollision->radius, 5, sf::Color(10, 10, 10), sf::Color(255, 0, 0), 3);
+	m_player->cShape->circle.setPosition(m_player->cTransform->pos.x, m_player->cTransform->pos.y);
+	m_player->cInput = std::make_shared<CInput>();
 }
 void Game::sEnemySpawner()
 {
